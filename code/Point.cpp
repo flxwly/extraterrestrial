8cb30e2fa3b03abd3f12ce85c386d359ff7009f3
flxwly <54081302+flxwly@users.noreply.github.com>
2020-06-24 18:58:40 +0200

    temp

--- code/Point.cpp
+++ code/Point.cpp
@@ -1,62 +1,43 @@
 #include "Point.hpp"
 
-struct Point::dist_PRIORITY {
-    bool operator()(std::pair<int, std::pair<int, int>> pobj, std::pair<int, std::pair<int, int>> cobj, Point *cur) {
-        return pobj.first < cobj.first;
-    }
-};
 
+Point::Point(int _x, int _y, std::vector<std::pair<int, int>> *_dp_areas, int _color) {
 
-double Point::get_dist(int rx, int ry) const {
-    return dist(this->x, rx, this->y, ry);
-}
-
-
-Point::Point(int _x, int _y, std::vector<std::pair<int, int>> *_dp_areas, unsigned int _color) {
-
-    this->x = _x;
-    this->y = _y;
-    this->color = static_cast<short int>(_color);
+    Point::x = _x;
+    Point::y = _y;
+    Point::color = static_cast<short int>(_color);
 
     // determine nearest deposit area
-    this->nearest_deposit_area = _dp_areas->front();
-    double t_dist = dist(this->nearest_deposit_area.first, this->x, this->nearest_deposit_area.second, this->y);
+    Point::closest_deposit_area = _dp_areas->front();
+    double t_dist = dist(Point::closest_deposit_area.first, Point::x, Point::closest_deposit_area.second, Point::y);
     for (auto dp_area : *_dp_areas) {
-        if (t_dist > dist(dp_area.first, this->x, dp_area.second, this->y)) {
-            t_dist = dist(dp_area.first, this->x, dp_area.second, this->y);
-            this->nearest_deposit_area = dp_area;
+        if (t_dist > dist(dp_area.first, Point::x, dp_area.second, Point::y)) {
+            t_dist = dist(dp_area.first, Point::x, dp_area.second, Point::y);
+            Point::closest_deposit_area = dp_area;
         }
     }
+}
 
+void Point::calculate_closest_points(std::vector<Point *> *_points) {
 
-    std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, Point::dist_PRIORITY> nearest_r_points;
-    std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, Point::dist_PRIORITY> nearest_c_points;
-    std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, Point::dist_PRIORITY> nearest_b_points;
-
-
-    int dist = 0;
-    for (auto _point : r_points) {
-        if (_point.first != this->x && _point.second != this->y) {
-            dist = sqrt(pow(this->x - _point.first, 2) + pow(this->y - _point.second, 2)); 
-            nearest_c_points.push(std::make_pair(dist, std::make_pair(_point.first, _point.second)));
+    for (auto _point : *_points) {
+        if (_point->x != Point::x && _point->y != Point::y) {
+            double t_dist = dist(Point::x, _point->x, Point::y, _point->y);
+            switch (_point->color) {
+                case 1:
+                    r_points.push({t_dist, _point});
+                    break;
+                case 2:
+                    c_points.push({t_dist, _point});
+                    break;
+                case 3:
+                    b_points.push({t_dist, _point});
+                    break;
+                default:
+                    break;
+            }
         }
     }
-
-    for(auto _point : c_points){
-        if(_point.first != this->x && _point.second != this->y){
-            dist = sqrt(pow(this->x - _point.first, 2) + pow(this->y - _point.second, 2)); 
-            nearest_c_points.push(std::make_pair(dist, std::make_pair(_point.first, _point.second)));
-        }
-    }
-
-    for(auto _point : b_points){
-        if(_point.first != this->x && _point.second != this->y){
-            dist = sqrt(pow(this->x - _point.first, 2) + pow(this->y - _point.second, 2)); 
-            nearest_b_points.push(std::make_pair(dist, std::make_pair(_point.first, _point.second)));
-        }
-    }
-
-
 }
 
 
--- code/Point.hpp
+++ code/Point.hpp
@@ -15,18 +15,27 @@
 
 class Point {
 public:
-    Point(int _x, int _y, std::vector<std::pair<int, int>> *dp_areas, unsigned int color);
+    Point(int _x, int _y, std::vector<std::pair<int, int>> *_dp_areas, int _color);
 
-    double get_dist(int rx, int ry) const;
+    void calculate_closest_points(std::vector<Point *> *_points);
 
 private:
     int x, y;
-    short int color = -1;
 
     //undef: -1, r: 1, c: 2, b: 3;
-    std::pair<int, int> nearest_deposit_area;
+    int color;
 
-    struct dist_PRIORITY;
+    std::pair<int, int> closest_deposit_area;
+
+    struct dist_PRIORITY {
+        bool operator()(std::pair<double, Point *> pobj, std::pair<double, Point *> cobj) {
+            return pobj.first < cobj.first;
+        }
+    };
+
+    std::priority_queue<std::pair<double, Point *>, std::vector<std::pair<double, Point *>>, Point::dist_PRIORITY> r_points;
+    std::priority_queue<std::pair<double, Point *>, std::vector<std::pair<double, Point *>>, Point::dist_PRIORITY> c_points;
+    std::priority_queue<std::pair<double, Point *>, std::vector<std::pair<double, Point *>>, Point::dist_PRIORITY> b_points;
 
 
 };
