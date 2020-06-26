316bf7e4203aebf39c1810f9a8e02a8876e8eb8e
flxwly <54081302+flxwly@users.noreply.github.com>
2020-06-26 22:18:14 +0200

    some changes
    Signed-off-by: flxwly <54081302+flxwly@users.noreply.github.com>

--- code/test.cpp
+++ code/test.cpp
@@ -1,6 +1,10 @@
 #include"Point.hpp"
 #include"MapData.hpp"
-#include"Robot.hpp"
+#include "Robot.hpp"
+
+MapData GAME1(360, 270, &GAME1REDPOINTS, &GAME1GREENPOINTS, &GAME1BLACKPOINTS, &GAME1DEPOSITAREAS, &GAME1WALLS,
+              &GAME1TRAPS, &GAME1SWAMPS);
+
 
 #include<iostream>
 #include<vector>
@@ -12,7 +16,7 @@ std::vector<Point> predefine_path(const int &posx, const int &posy){
     //TODO update point choose method
     std::vector<Point> _path;
     int i = 0;
-    std::vector<Point*> points = MapData::getAllPoints;
+    std::vector<Point*> points = GAME1.getAllPoints();
     _path.push_back(*points.front());
     for(auto _point : points){
         if(i>0){
@@ -26,21 +30,21 @@ std::vector<Point> predefine_path(const int &posx, const int &posy){
     std::array<int,3> obj;
     obj = Robot::get_loaded_objects();
     int num = Robot::get_inventory_number();
-    while(num < 6){
+    while(num < 6) {
         if(obj[2] < 2){
-            temp = _path.back()->get_closest_point(3);
+            temp = _path.back().get_closest_point(3);
             _path.push_back(*temp);
             obj[2]++;
             num++;
         }
         if(obj[1] < 2){
-            temp = _path.back()->get_closest_point(2);
+            temp = _path.back().get_closest_point(2);
             _path.push_back(*temp);
             obj[1]++;
             num++;
         }
         if(obj[0] < 2){
-            temp = _path.back()->get_closest_point(1);
+            temp = _path.back().get_closest_point(1);
             _path.push_back(*temp);
             obj[1]++;
             num++;
